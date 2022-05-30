18234c1a46180b49fbdf0f4977789227fbd0d67b
Wren <renprofessional97@gmail.com>
2022-05-30 14:22:55 +0100

    mod file

--- OOP/exp1.js
+++ OOP/exp1.js
@@ -1,4 +1,5 @@
 class Person {
+  // constructor function constructs our new object and sets the properties and methods
   constructor(name, age, job) {
     //properties here
     this.name = name;
@@ -15,7 +16,11 @@ class Person {
     console.log(`I am going to build a house, because I am a ${this.job}`);
   }
 }
+//  creating a new INSTANCE of the class.
 const dave = new Person("Dave", 41, "Builder");
-
+// the 'new' keywork
+// 1 - creates an empty object
+// 2 - binds the value of this to the new empty object
+// 3 - calls the constructor function to build the new object
 dave.talks();
 dave.work();
